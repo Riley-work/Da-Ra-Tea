f1bf839916359a5aefa0ed58d1b0f397277dd7bc
Riley-work <rileyswiftemer@gmail.com>
2024-09-24 17:54:16 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Da-Ra-Tea';
+  firstName = 'Damini';
+  imagepath="assests/thu/jpg";
+
+  passValueToComponent(e:any) {
+    console.log(e.target.value);
+  }
+
+  getValue(myvalue:any) {
+    console.log = myvalue.target.value;
+  }
+  defaultV:string ='type here';
+
+  public display = false;
+  
+  isLoggedIn: boolean = true;
+username: string = 'Damiii';
+
+isApproved = false;
+
+approve(code: boolean){
+  this.isApproved = code;
+
+}
+
+
+}
